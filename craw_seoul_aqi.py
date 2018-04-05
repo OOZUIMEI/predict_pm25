86147bdb15b47b8de23ee3c1f7a137f021fbc4c4
Alex Bui <cuongbt91@gmail.com>
2018-04-05 10:15:23 +0900

    add crawl all district

--- craw_seoul_aqi.py
+++ craw_seoul_aqi.py
@@ -18,10 +18,37 @@ def mine_data(html):
             txt = v.get_text()
             if "-" not in txt:
                 values.append(txt.encode('ascii', 'ignore'))
-        values = [float(x) for x in values[1:]]
+        values = [float(x) for x in values[1:7]]
         values.append(utils.AQIPM10(values[0]))
         values.append(utils.AQIPM25(values[1]))
     return values
+
+
+def mine_all_area(html):
+    tables = html.find('table', attrs={"class": "tbl2"})
+    body = tables.find('tbody')
+    all_values = []
+    if body:
+        all_tr = body.find_all("tr")
+        if all_tr:
+            for ku, r in enumerate(all_tr):
+                td = r.find_all('td')
+                values = []
+                for i, v in enumerate(td):
+                    txt = v.get_text()
+                    txt = "".join(txt.replace("\n", "").split(" "))
+                    if i and "-" not in txt:
+                        txt = txt.encode('ascii', 'ignore')
+                        if txt:
+                            values.append(float(txt))
+                        else:
+                            values.append(all_values[0][i])
+                    elif i == 0:                
+                        values.append(ku)
+                values.append(utils.AQIPM10(values[1]))
+                values.append(utils.AQIPM25(values[2]))
+                all_values.append(values)
+    return all_values
 
 
 # format hour, day < 10 to 10 format
@@ -83,13 +110,19 @@ if __name__ == "__main__":
     parser.add_argument("-si", "--save_interval", default=48, type=int)
     parser.add_argument("-s", "--start", default="2008-01-01 01:00:00", type=str)
     parser.add_argument("-e", "--end", type=str)
+    parser.add_argument("-a", "--get_all", type=int, default=0)
     
     args = parser.parse_args()
     if args.test:
         with open("test_seoul_aqi.html") as file:
             html = Soup(file.read(), "html5lib")
-            # PM-10(㎍/㎥),PM-2.5(㎍/㎥),O3(ppm),NO2(ppm),CO(ppm),SO2(ppm)
-            mine_data(html)
+            # timestamp,K,PM-10(㎍/㎥),PM-2.5(㎍/㎥),O3(ppm),NO2(ppm),CO(ppm),SO2(ppm)
+            # mine_data(html)
+            values = mine_all_area(html)
+            timestamp = "2008-01-01 01:00:00"
+            output = ""
+            for x in values:
+                output += timestamp + "," + utils.array_to_str(x, ",") + "\n"
     else:
         filename = "craw_seoul_aqi_%s_%s.txt" % (clear_datetime(args.start), clear_datetime(args.end))
         start = datetime.strptime(args.start, pr.fm)
@@ -120,13 +153,19 @@ if __name__ == "__main__":
                 try:
                     counter += 1
                     html = craw_data(year, month, date, hour)
-                    data = mine_data(html)
-                    if data:
-                        output += start.strftime(pr.fm) + "," + utils.array_to_str(data, ",") + "\n"
-                    if (counter - last_save) == save_interval:
-                        last_save = counter
-                        write_log(filename, output)
-                        output = ""
+                    # data = mine_data(html)
+                    if  args.get_all:
+                        data = mine_all_area(html)
+                        for x in data:
+                            output += start.strftime(pr.fm) + "," + utils.array_to_str(x, ",") + "\n"
+                    else:
+                        data = mine_data(html)
+                        if data:
+                            output += start.strftime(pr.fm) + "," + utils.array_to_str(data, ",") + "\n"
+                        if (counter - last_save) == save_interval:
+                            last_save = counter
+                            write_log(filename, output)
+                            output = ""
                 except Exception as e:
                     print(start.strftime(pr.fm), e)
                 start = start + timedelta(hours=1)
