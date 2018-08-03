aff53fa485241d259cbf5542970c0c75459f7a98
Alex Bui <cuongbt91@gmail.com>
2018-08-03 16:04:02 +0900

    change style summary

--- mask_gan.py
+++ mask_gan.py
@@ -202,8 +202,8 @@ class MaskGan(BaselineModel):
             if self.use_attention:
                 feed[self.attention_inputs] = ct_t
 
-            summary, gen_loss, dis_loss, critic_loss, pred, _, _, _= session.run(
-                [self.merged, self.gen_loss, self.dis_loss, self.critic_loss, self.outputs, self.gen_op, 
+            gen_loss, dis_loss, critic_loss, pred, _, _, _= session.run(
+                [self.gen_loss, self.dis_loss, self.critic_loss, self.outputs, self.gen_op, 
                 self.dis_op, self.critic_op], feed_dict=feed)
 
             total_gen_loss.append(gen_loss) 
@@ -221,8 +221,9 @@ class MaskGan(BaselineModel):
 
         total_gen_loss, total_dis_loss, total_critic_loss = np.mean(total_gen_loss), np.mean(total_dis_loss), np.mean(total_critic_loss)
         if train_writer is not None:
+            summary = tf.Summary()
+            summary.value.add(tag= "Generator Loss", simple_value=total_gen_loss)
+            summary.value.add(tag= "Discriminator Loss", simple_value=total_dis_loss)
+            summary.value.add(tag= "Critic Loss", simple_value=total_critic_loss)
             train_writer.add_summary(summary, num_epoch)
-            train_writer.add_summary(summary, total_gen_loss)
-            train_writer.add_summary(summary, total_dis_loss)
-            train_writer.add_summary(summary, total_critic_loss)
         return total_gen_loss, total_dis_loss, total_critic_loss, preds
