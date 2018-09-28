45a4308a820fe9e8c68e66793dc1f66a1ae01688
Alex Bui <cuongbt91@gmail.com>
2018-09-28 17:12:46 +0900

    fix clip norm

--- mask_gan.py
+++ mask_gan.py
@@ -214,7 +214,8 @@ class MaskGan(BaselineModel):
 
     def get_optimization(self, loss, name_scope):
         vars_ = [v for v in tf.trainable_variables() if v.op.name.startswith(name_scope)]
-        grads = tf.gradients(loss, vars_)
+        grads = self.optimizer.compute_gradients(loss, vars_)
+        # grads = tf.gradients(loss, vars_)
         return grads, vars_
 
     # using stride to reduce the amount of data to loop over time intervals
@@ -296,9 +297,11 @@ class MaskGan(BaselineModel):
             for g, _ in grad_and_vars:
                 expanded_g = tf.expand_dims(g, 0)
                 grads.append(expanded_g)
+            print(grad_and_vars[0].get_shape())
             grad = tf.concat(axis=0, values=grads)
             grad = tf.reduce_mean(grad, 0)
-            v = grad_and_vards[0][1]
+            print(grad.get_shape())
+            v = grad_and_vars[0][1]
             grad_and_var = (grad, v)
             average_grads.append(grad_and_var)
         return average_grads
