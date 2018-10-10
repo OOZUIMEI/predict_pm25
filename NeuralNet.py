fix gradients

NeuralNet.py
@@ -21,8 +21,8 @@ class NeuralNetwork(object):
         self.decoder_vector_size = decoder_vector_size
         self.attention_vector_size = attention_vector_size
         self.learning_rate = learning_rate
-        self.initializer = tf.contrib.layers.xavier_initializer()
         self.dtype = dtype
+        self.initializer = tf.contrib.layers.xavier_initializer()
     
     def add_placeholders(self):
         self.embedding = tf.Variable([], validate_shape=False, dtype=tf.float32, name="Data_Embedding", trainable=False)

baseline_cnnlstm.py
@@ -32,7 +32,7 @@ class BaselineModel(object):
         self.batch_size = batch_size
         self.grid_square = grid_size * grid_size
         self.loss = loss
-        self.dropout = 0.9
+        self.dropout = 0.0
         self.df_ele = df_ele
         self.dtype = dtype        
         self.map = heatmap.build_map()
@@ -67,7 +67,7 @@ class BaselineModel(object):
                 self.e_params["fw_cell_size"] = self.districts
         self.use_gen_cnn = False
         self.mtype = 4
-        self.use_batch_norm = True
+        self.use_batch_norm = False
     
     def set_training(self, training):
         self.is_training = training
@@ -128,7 +128,9 @@ class BaselineModel(object):
                 cnn = tf.layers.flatten(cnn)
                 cnn_shape = cnn.get_shape()
                 # last_dim = cnn_shape[-1] * cnn_shape[-2]
-                last_dim = int(cnn_shape[-1]) / self.encoder_length
+                print(cnn_shape)
+                # last_dim = int(cnn_shape[-1]) / self.encoder_length
+                last_dim = int(cnn_shape[-1])
                 enc_data = tf.reshape(cnn, [self.batch_size, self.encoder_length, int(last_dim)])
                 # enc_data = tf.unstack(enc_data, axis=1)
             else:
@@ -182,7 +184,7 @@ class BaselineModel(object):
     def get_attention_rep(self, inputs):
         with tf.variable_scope("attention_rep", initializer=self.initializer, reuse=tf.AUTO_REUSE):
             params = {
-                "fw_cell": self.e_params["fw_cell"],
+                "fw_cell": "cudnn_lstm",
                 "fw_cell_size": self.rnn_hidden_units
             }
             inputs.set_shape((self.batch_size, self.attention_length, self.atttention_hidden_size))

evaluate.py
@@ -34,7 +34,7 @@ def evaluate(pred, labs, rg, is_classify=False, verbose=True):
             print("r2_score:%.2f" % r2)
 
 
-def evaluate_sp(url, url2, is_grid=False):
+def evaluate_sp(url, url2, is_grid=True):
     map_ = heatmap.build_map()
     data = utils.load_file(url)
     if type(data) is list:
@@ -61,7 +61,7 @@ def evaluate_sp(url, url2, is_grid=False):
             x_l = heatmap.fill_map(x, map_)
             lbg.append(x_l)
         lbg = np.asarray(lbg)
-        lbg = lbg.flatten()
+        lbg = lbg.flatten() * 300
         pred_t  = []
         if is_grid:
             for d_ in d:
@@ -71,16 +71,16 @@ def evaluate_sp(url, url2, is_grid=False):
             for d_ in d:
                 d_t = heatmap.fill_map(d_, map_)
                 pred_t.append(d_t)
-        pred_t = np.asarray(pred_t)
+        pred_t = np.asarray(pred_t) * 300
         pred_t = pred_t.flatten()
         mse = mean_squared_error(lbg, pred_t)
         loss_mse += mse
         loss_mae += mean_absolute_error(lbg, pred_t)
         loss_rmse += sqrt(mse)
         utils.update_progress((i + 1.0) / dtl)
-    loss_mse = loss_mse / lt * 300
-    loss_mae = loss_mae / lt * 300
-    loss_rmse = loss_rmse / lt * 300
+    loss_mse = loss_mse / lt
+    loss_mae = loss_mae / lt
+    loss_rmse = loss_rmse / lt
     print("MSE: %.2f" % loss_mse)
     print("MAE: %.2f" % loss_mae)
     print("RMSE: %.2f" % loss_rmse)
@@ -93,7 +93,6 @@ def evaluate_reg(url, url2):
     preds = np.reshape(preds.flatten(), (lt, preds.shape[2], preds.shape[-1]))
     labels = utils.load_file(url2)
     labels = np.array(labels)
-    loss_mse = 0.0
     loss_mae = 0.0
     loss_rmse = 0.0
     for i, d in enumerate(preds):
@@ -102,16 +101,12 @@ def evaluate_reg(url, url2):
         lbg = np.array(lbt)
         lbg = lbg.flatten()
         pred_t = d.flatten()
-        plt_l = len(pred_t)
         mse = mean_squared_error(lbg, pred_t)
-        loss_mse += mse
         loss_mae += mean_absolute_error(lbg, pred_t)
         loss_rmse += sqrt(mse)
 
-    loss_mse = loss_mse / lt * 300
     loss_mae = loss_mae / lt * 300
     loss_rmse = loss_rmse / lt * 300
-    print("MSE: %.2f" % loss_mse)
     print("MAE: %.2f" % loss_mae)
     print("RMSE: %.2f" % loss_rmse)
 

mask_gan.py
@@ -62,7 +62,7 @@ class MaskGan(BaselineModel):
         # critic loss
         self.use_critic = use_critic
         # set up multiple cnns layers to generate outputs
-        self.use_gen_cnn = True
+        self.use_gen_cnn = False
         # add l1 norm to loss
         self.use_l1 = False
         self.is_clip = True
@@ -105,7 +105,8 @@ class MaskGan(BaselineModel):
             tanh_outputs = None
             if self.use_gen_cnn:
                 tanh_outputs = tf.tanh(outputs)
-
+            else:
+                tanh_outputs = outputs
         return enc_output, dec, outputs, tanh_outputs, estimated_values, attention
 
     #perform decoder with critic estimated award
@@ -200,44 +201,38 @@ class MaskGan(BaselineModel):
     def train_discriminator(self, loss):
         with tf.name_scope("train_discriminator"):
             dis_grads, dis_vars = self.get_optimization(loss, "discriminator")
-            dis_grads = tf.clip_by_global_norm(dis_grads, 10.)
-            dis_train_op = self.optimizer.apply_gradients(zip(dis_grads, dis_vars))
+            dis_grads, _ = tf.clip_by_global_norm(dis_grads, 10.)
+            dis_optimizer = tf.train.AdamOptimizer(self.learning_rate, self.beta1)
+            dis_train_op = dis_optimizer.apply_gradients(zip(dis_grads, dis_vars))
             return dis_train_op
     
     # policy gradient
     def train_generator(self, loss):
         with tf.name_scope("train_generator"):
-            if self.use_l1:
-                l1_norm = tf.contrib.layers.l1_regularizer(scale=self.lamda)
-                l1_loss = tf.contrib.layers.apply_regularization(l1_norm, gen_vars)
-                loss += l1_loss
-            gen_grads, gen_vars = self.get_optimization(loss, "generator")
-            if self.is_clip:
-                gen_grads = tf.clip_by_global_norm(gen_grads, 10.)
-            # if self.gen_loss_type == 0:
-            #     # gradient ascent, maximum reward  => descent with minimizing the loss
-            #     gen_grads = tf.gradients(-loss, gen_vars)
-            # else:
-            #     # using mse without critic
-            #     gen_grads = tf.gradients(loss, gen_vars)
-            gen_train_op = self.optimizer.apply_gradients(zip(gen_grads, gen_vars))
+            gen_vars = [v for v in tf.trainable_variables() if v.op.name.startswith("generator")]
+            gen_optimizer = tf.train.AdamOptimizer(self.learning_rate, self.beta1) 
+            if self.gen_loss_type == 0:
+                gen_grads = tf.gradients(-loss, gen_vars)
+            else:
+                gen_grads = tf.gradients(loss, gen_vars)
+            gen_grads, _ = tf.clip_by_global_norm(gen_grads, 10.)
+            gen_train_op = gen_optimizer.apply_gradients(zip(gen_grads, gen_vars))
             return gen_train_op
 
     def get_optimization(self, loss, name_scope):
         vars_ = [v for v in tf.trainable_variables() if v.op.name.startswith(name_scope)]
-        grads = self.optimizer.compute_gradients(loss, vars_)
-        # grads = tf.gradients(loss, vars_)
+        #grads = self.optimizer.compute_gradients(loss, vars_)
+        grads = tf.gradients(loss, vars_)
         return grads, vars_
 
     # using stride to reduce the amount of data to loop over time intervals
     def run_epoch(self, session, data, num_epoch=0, train_writer=None, verbose=True, train=False, shuffle=True, stride=4):
         st = time.time()
         if not train:
-            train_op = tf.no_op()
+            self.gen_op = tf.no_op()
+            self.dis_op = tf.no_op()
         dt_length = len(data)
         # print("data_size: ", dt_length)
-        cons_b = self.batch_size * stride
-        total_steps = dt_length // cons_b
         total_gen_loss = []
         total_dis_loss = []
         total_critic_loss = []
@@ -246,6 +241,15 @@ class MaskGan(BaselineModel):
         if shuffle:
             r = np.random.permutation(dt_length)
             ct = ct[r]
+      
+        strides = [4, 8, 12]
+        if train:
+            np.random.shuffle(strides)
+            stride = strides[0]
+      
+        cons_b = self.batch_size * stride
+
+        total_steps = dt_length // cons_b
         for step in xrange(total_steps):
             index = range(step * cons_b,
                           (step + 1) * cons_b, stride)

rnn_utils.py
@@ -50,6 +50,7 @@ def execute_sequence(inputs, params):
         if "cudnn" in params["fw_cell"]:
             outputs, fn_state = fw_cell(inputs)
             if params["fw_cell"] == "cudnn_lstm":
+                c_shape = fn_state[0].get_shape()
                 if c_shape[0] == 1:
                     c = tf.squeeze(fn_state[1], [0])
                     h = tf.squeeze(fn_state[0], [0])
@@ -271,8 +272,8 @@ def get_cnn_rep(cnn_inputs, mtype=4, activation=tf.nn.relu, max_filters=8, use_b
         # normalize input to [-1, 1] in generator
         cnn_inputs = tf.tanh(cnn_inputs)
         # 25 x 25 x H => 8x8x8 => 4x4x8
-        conv1 = get_cnn_unit(cnn_inputs, max_filters, (11,11), activation, "VALID", "rep_conv1", use_batch_norm, dropout)
-        cnn_outputs = get_cnn_unit(conv1, max_filters, upscale_k, activation, "SAME", "rep_conv2", use_batch_norm, dropout)
+        conv1 = get_cnn_unit(cnn_inputs, max_filters, (11,11), activation, "VALID", "rep_conv1")
+        cnn_outputs = get_cnn_unit(conv1, max_filters, upscale_k, activation, "SAME", "rep_conv2")
     # else:
     #     """
     #         Use for representation steps of both encoder and decoder

stack_autoencoder.py
@@ -1,10 +1,9 @@
 from __future__ import print_function
-import sys 
+import sys
 import numpy as np
 import tensorflow as tf
 import properties as pr
 from adain import Adain
-
 
 # reference: deep learning architecture for air quality predictions
 # hidden layer = 300
@@ -12,7 +11,7 @@ from adain import Adain
 # time intervals in the paper 
 class StackAutoEncoder(Adain):
     
-    def __init__(self, pre_train=False, learning_rate=0.01, **kwargs):
+    def __init__(self, pre_train=False, learning_rate=0.01, **kwargs): 
         super(StackAutoEncoder, self).__init__(**kwargs)
         self.pre_train_iter = 10
         self.time_intervals = 8
@@ -59,7 +58,7 @@ class StackAutoEncoder(Adain):
                 ae_loss = tf.losses.mean_squared_error(labels=inputs, predictions=output_ae)
                 train_weights = []
                 for x in tf.trainable_variables():
-                    if x.op.name.startsWith(scope_name):
+                    if x.op.name.startswith(scope_name):
                         train_weights.append(x)
                         if "bias" not in x.name.lower():
                             ae_loss += tf.nn.l2_loss(x)