629559341a1856743789bb12e882fea79c4a7385
cuongbt91 <cuongbt91@gmail.com>
2018-12-04 18:57:48 +0900

    fix srcn model

--- srcns.py
+++ srcns.py
@@ -5,10 +5,10 @@ import rnn_utils
 
 #APNet for transportation data
 
-class SVCNs(TNetLSTM):
+class SRCN(TNetLSTM):
 
     def __init__(self, **kwargs):
-        super(SVCNs, self).__init__(**kwargs)
+        super(SRCN, self).__init__(**kwargs)
         # alpha is used for generator loss function
 
     # generator cnn layers
@@ -18,12 +18,12 @@ class SVCNs(TNetLSTM):
         # input (64, 32, 32, 16) output (64, 16, 16, 16)
         msf1_down = rnn_utils.get_maxpool(msf1, (2,2), padding="SAME", name="down_sample_1", strides=(2,2))
         # input (64, 16, 16, 16) output (64, 16, 16, 32)
-        msf2 = rnn_utils.get_cnn_unit(msf1_down, 32, (3,3), activation, padding="SAME", name="sample_1", strides=(1,1))
+        msf2 = rnn_utils.get_cnn_unit(msf1_down, 32, (3,3), activation, padding="SAME", name="sample_2", strides=(1,1))
         # input (64, 16, 16, 32) output (64, 8, 8, 32)
         msf2_down = rnn_utils.get_maxpool(msf2, (2,2), padding="SAME", name="down_sample_2", strides=(2,2))
         # input (64, 8, 8, 32) output (64, 8, 8, 128)
-        msf3 = rnn_utils.get_cnn_unit(msf2_down, 64, (3,3), activation, padding="SAME", name="sample_1", strides=(1,1))
-        msf32 = rnn_utils.get_cnn_unit(msf3, 64, (3,3), activation, padding="SAME", name="sample_1", strides=(1,1))
-        msf3_down = rnn_utils.get_maxpool(msf32, (2,2), padding="SAME", name="down_sample_2", strides=(2,2))
+        msf3 = rnn_utils.get_cnn_unit(msf2_down, 64, (3,3), activation, padding="SAME", name="sample_3", strides=(1,1))
+        msf32 = rnn_utils.get_cnn_unit(msf3, 64, (3,3), activation, padding="SAME", name="sample_4", strides=(1,1))
+        msf3_down = rnn_utils.get_maxpool(msf32, (2,2), padding="SAME", name="down_sample_4", strides=(2,2))
         msf3_ = tf.layers.flatten(msf3_down)
         return msf3_
